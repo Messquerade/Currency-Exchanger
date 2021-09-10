d2b644c19b75feb69ccb9c2d022191fac581c83e
Messquerade <annac.klingberg@gmail.com>
2021-09-10 10:49:37 -0700

    add successful API call and catch errors

--- src/exchange.service.js
+++ src/exchange.service.js
@@ -1,5 +1,13 @@
 export default class CurrencyExchange {
-  static exchange(baseCurrency) {
-    
+  static async exchange(baseCurrency) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`);
+      if (!response.ok) {
+        throw Error(response.status);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,33 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import HelloWorld from "./business";
+import CurrencyExchange from "./exchange.service";
 
-$("#click-me").click(function () {
-  let message = HelloWorld();
-  $("h2").text(message);
+
+function getConversionRates(response) {
+  if (response) {
+    $('#choose-exchange').show();
+    $('#choose-base').hide();
+    return response;
+  } else if (response === "404") {
+    $('#base-show-errors').text("The code you entered does not match any of our supported currencies");
+  } else {
+    $('#base-show-errors').text(`There was an error processing your request: ${response}`);
+  }
+}
+
+async function makeApiCall(baseCurrency) {
+  const response = await CurrencyExchange.exchange(baseCurrency);
+  return getConversionRates(response);
+}
+
+$(document).ready(function () {
+  $('#base-currency-form').submit(function(event) {
+    event.preventDefault();
+    let baseCurrency = $('#base-currency').val();
+    let response = makeApiCall(baseCurrency);
+    console.log(response);
+  });
+
+
 });
